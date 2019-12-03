6bb691a841deb0268e51411e06bdfc9a17a72104
kierepkae <kierepkaedyta@gmail.com>
2019-12-03 10:03:16 +0000

    handling exception for dividing by 0

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace ExceptionHandling
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ExceptionHandling {
+    class Program {
+        static void Main (string[] args) {
+            try {
+                var calculator = new Calculator ();
+                var result = calculator.Divide (5, 0);
+            } catch (System.Exception) {
+
+                System.Console.WriteLine ("Sorry, an unexpected error accured.");
+            }
+
         }
     }
 }
